8b3228b7a4749430c5aaa8c8158970d0ae85072d
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2021-10-04 17:58:21 +0700

    onclite: adapt ANT+ with CAF
    Signed-off-by: Hadad <hadad@linuxmail.org>

--- device.mk
+++ device.mk
@@ -106,10 +106,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_PACKAGES += \
     AntHalService \
     libantradio \
-    com.dsi.ant.antradio_library
-
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
+    antradio_app
 
 # Audio
 PRODUCT_PACKAGES += \
