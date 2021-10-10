40d1e80b2db69aad8d135e019ed06e680afd6fa1
Hadad <hadad@linuxmail.org>
2021-10-10 14:26:52 +0700

    onclite: update Performance stack from LA.UM.9.6.2.r1-03600-89xx.0.
    Change-Id: I72b388f1c6cebde9f3cf577a9c12102748309df7

--- device.mk
+++ device.mk
@@ -364,7 +364,8 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
     power.qcom \
     vendor.qti.hardware.perf@2.0.vendor \
-    vendor.qti.hardware.perf@2.1.vendor
+    vendor.qti.hardware.perf@2.1.vendor \
+    vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf \
