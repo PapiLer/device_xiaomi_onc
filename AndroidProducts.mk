cd2294befb0dddceded1b20142e8b24750247c83
Risti699 <wahhap10@gmail.com>
2021-10-26 16:32:31 +0000

    SSOS changes
    Signed-off-by: Risti699 <wahhap10@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xdroid_onclite.mk
+    $(LOCAL_DIR)/ssos_onclite.mk
 
 COMMON_LUNCH_CHOICES := \
-    xdroid_onclite-user \
-    xdroid_onclite-userdebug
+    ssos_onclite-user \
+    ssos_onclite-userdebug
